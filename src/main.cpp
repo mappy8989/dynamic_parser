fae87c49b6019dad5b8d0d5cc927909c9bf4072a
IgorF <kleef@mail.ru>
2025-05-04 20:30:23 +0300

    Add tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include "scan.hpp"
+#include <cassert>
 #include <iostream>
+#include <string_view>
 
 int main() {
-  stdx::foo();
-  std::cout << "g g f!" << std::endl;
+  // All params ok test
+  const std::string input_model = "Hello 43 55 wewe 5.67 degrees";
+  const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
+
+  auto scan_res =
+      stdx::scan<int, int, std::string, float>(input_model, format_model);
+  assert(scan_res.has_value() && "All params ok");
+
+  // Incorrect param test
+  std::string format = "Hello {%d} {%d} {%s} {%gf} degrees";
+
+  scan_res = stdx::scan<int, int, std::string, float>(input_model, format);
+  assert(!scan_res.has_value() && "Incorrect param");
+
+  // Incorrect input test
+  std::string input = "Hello 43 55 wewe some degrees";
+
+  scan_res = stdx::scan<int, int, std::string, float>(input, format_model);
+  assert(!scan_res.has_value() && "Incorrect input");
+
   return 0;
 }
