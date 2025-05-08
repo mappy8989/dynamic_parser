6f5d98bd9c2dfbfaf22266e722e2362afae09f60
IgorF <kleef@mail.ru>
2025-05-08 11:15:38 +0300

    clang formatted

--- include/types.hpp
+++ include/types.hpp
@@ -8,15 +8,16 @@ namespace stdx::details {
 // Класс для хранения ошибки неуспешного сканирования
 
 struct scan_error {
-  std::string message;
+    std::string message;
 };
 
 // Шаблонный класс для хранения результатов успешного сканирования
 
-template <typename... Ts> struct scan_result {
-  // здесь ваш код
-  std::tuple<Ts...> result;
-  std::tuple<Ts...> values(void) { return result; }
+template <typename... Ts>
+struct scan_result {
+    // здесь ваш код
+    std::tuple<Ts...> result;
+    const std::tuple<Ts...> &values(void) { return result; }
 };
 
-} // namespace stdx::details
+}  // namespace stdx::details
--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -6,14 +6,13 @@
 TEST(ScanTest, All_params_ok_1) {
     const std::string input_model = "Hello 43 55 wewe 5.67 degrees";
     const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
-  
-    auto scan_res =
-        stdx::scan<int, int, std::string, float>(input_model, format_model);
+
+    auto scan_res = stdx::scan<int, int, std::string, float>(input_model, format_model);
     ASSERT_TRUE(scan_res.has_value());
 }
 
 TEST(ScanTest, Incorrect_param_1) {
-    const std::string input_model = "Hello 43 55 wewe 5.67 degrees";  
+    const std::string input_model = "Hello 43 55 wewe 5.67 degrees";
     const std::string format = "Hello {%d} {%d} {%s} {%gf} degrees";
 
     auto scan_res = stdx::scan<int, int, std::string, float>(input_model, format);
@@ -21,13 +20,12 @@ TEST(ScanTest, Incorrect_param_1) {
 }
 
 TEST(ScanTest, Incorrect_input_1) {
-    const std::string input = "Hello 43 55 wewe some degrees";  
+    const std::string input = "Hello 43 55 wewe some degrees";
     const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
 
     auto scan_res = stdx::scan<int, int, std::string, float>(input, format_model);
     ASSERT_FALSE(scan_res.has_value());
 }
-
 
 TEST(ScanTest, Incorrect_input_2) {
     auto input = "Hello some 55 wewe 5.67 degrees";
@@ -40,7 +38,7 @@ TEST(ScanTest, Incorrect_input_2) {
 TEST(ScanTest, All_params_ok_2) {
     auto input = "123";
     auto format = "{%d}";
-  
+
     auto scan_res = stdx::scan<int>(input, format);
     ASSERT_TRUE(scan_res.has_value());
 }
@@ -48,7 +46,7 @@ TEST(ScanTest, All_params_ok_2) {
 TEST(ScanTest, Incorrect_param_2) {
     auto input = "123 123.56 456";
     auto format = "{%d} {%d} {%d}";
-  
+
     auto scan_res = stdx::scan<int, int, int>(input, format);
     ASSERT_FALSE(scan_res.has_value());
 }
@@ -56,7 +54,7 @@ TEST(ScanTest, Incorrect_param_2) {
 TEST(ScanTest, Incorrect_input_3) {
     auto input = "123 123.56 456";
     auto format = "{%d} {%s} {%d}";
-  
+
     auto scan_res = stdx::scan<int, int, int>(input, format);
     ASSERT_FALSE(scan_res.has_value());
 }
@@ -64,22 +62,21 @@ TEST(ScanTest, Incorrect_input_3) {
 TEST(ScanTest, Too_few_template_parameters) {
     auto input = "Hello world";
     auto format = "{%s} {%s}";
-  
+
     ASSERT_ANY_THROW((stdx::scan<std::string, std::string, int>(input, format)));
 }
 
 TEST(ScanTest, Too_much_template_parameters) {
     auto input = "Hello world !!!";
     auto format = "{%s} {%s}";
-  
+
     ASSERT_ANY_THROW((stdx::scan<std::string, std::string, int>(input, format)));
 }
 
 TEST(ScanTest, All_params_ok_3) {
     auto input = "-123 123";
     auto format = "{%d} {%u}";
-  
+
     auto scan_res = stdx::scan<int, uint8_t>(input, format);
     ASSERT_TRUE(scan_res.has_value());
 }
-
