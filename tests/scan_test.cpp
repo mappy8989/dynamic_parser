09eb55a57c1b9184563a603c9431585444ed66ff
IgorF <kleef@mail.ru>
2025-05-09 23:06:10 +0300

    Refactor tests

--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -7,32 +7,36 @@ TEST(ScanTest, All_params_ok_1) {
     const std::string input_model = "Hello 43 55 wewe 5.67 degrees";
     const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
 
-    auto scan_res = stdx::scan<int, int, std::string, float>(input_model, format_model);
-    ASSERT_TRUE(scan_res.has_value());
+    auto scan_res = stdx::scan<int, int, std::string_view, float>(input_model, format_model);
+    ASSERT_TRUE(scan_res.has_value() && (std::get<0>(scan_res.value().result) == 43) &&
+                (std::get<1>(scan_res.value().result) == 55 &&
+                 (std::get<2>(scan_res.value().result) == "wewe") &&
+                 (std::get<3>(scan_res.value().result) == 5.67f) &&
+                 (std::tuple_size_v<decltype(scan_res.value().result)> == 4)));
 }
 
 TEST(ScanTest, Incorrect_param_1) {
     const std::string input_model = "Hello 43 55 wewe 5.67 degrees";
     const std::string format = "Hello {%d} {%d} {%s} {%gf} degrees";
 
-    auto scan_res = stdx::scan<int, int, std::string, float>(input_model, format);
-    ASSERT_FALSE(scan_res.has_value());
+    auto scan_res = stdx::scan<int, int, std::string_view, float>(input_model, format);
+    ASSERT_TRUE(scan_res.error().message == "Unexpected type format %gf");
 }
 
 TEST(ScanTest, Incorrect_input_1) {
     const std::string input = "Hello 43 55 wewe some degrees";
     const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
 
-    auto scan_res = stdx::scan<int, int, std::string, float>(input, format_model);
-    ASSERT_FALSE(scan_res.has_value());
+    auto scan_res = stdx::scan<int, int, std::string_view, float>(input, format_model);
+    ASSERT_TRUE(scan_res.error().message == "Invalid argument");
 }
 
 TEST(ScanTest, Incorrect_input_2) {
     auto input = "Hello some 55 wewe 5.67 degrees";
     const std::string format_model = "Hello {%d} {%d} {%s} {%f} degrees";
 
-    auto scan_res = stdx::scan<int, int, std::string, float>(input, format_model);
-    ASSERT_FALSE(scan_res.has_value());
+    auto scan_res = stdx::scan<int, int, std::string_view, float>(input, format_model);
+    ASSERT_TRUE(scan_res.error().message == "Invalid argument");
 }
 
 TEST(ScanTest, All_params_ok_2) {
@@ -48,7 +52,7 @@ TEST(ScanTest, Incorrect_param_2) {
     auto format = "{%d} {%d} {%d}";
 
     auto scan_res = stdx::scan<int, int, int>(input, format);
-    ASSERT_FALSE(scan_res.has_value());
+    ASSERT_TRUE(scan_res.has_value());
 }
 
 TEST(ScanTest, Incorrect_input_3) {
@@ -63,14 +67,14 @@ TEST(ScanTest, Too_few_template_parameters) {
     auto input = "Hello world";
     auto format = "{%s} {%s}";
 
-    ASSERT_ANY_THROW((stdx::scan<std::string, std::string, int>(input, format)));
+    ASSERT_FALSE((stdx::scan<std::string_view, std::string_view, int>(input, format)));
 }
 
 TEST(ScanTest, Too_much_template_parameters) {
     auto input = "Hello world !!!";
     auto format = "{%s} {%s}";
 
-    ASSERT_ANY_THROW((stdx::scan<std::string, std::string, int>(input, format)));
+    ASSERT_FALSE((stdx::scan<std::string_view, std::string_view, int>(input, format)));
 }
 
 TEST(ScanTest, All_params_ok_3) {
